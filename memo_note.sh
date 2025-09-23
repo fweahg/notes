a6adf64dc028066418bc267f8d466f7ed383ed35
fw <65058888+fweahg@users.noreply.github.com>
2025-09-23 21:00:03 +0200

    Create memo_note.sh

---
+++ memo_note.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z $1 ];then
+ echo -n filename:
+ read -r f;
+else
+ f=$1;
+fi
+echo -n input:
+read -r line
+echo -e " file : ${f} "
+cat << EOF > $f
+`ls -l {$0,$1}`
+$line
+EOF
+cat $f
